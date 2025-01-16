afa5ab66b5c607dfa92855a356682b3967ac703e
Temitope <adegtun@yahoo.com>
2025-01-16 22:54:20 +0100

    Adding navigation to Sign up and Sign in pages

--- lib/features/auth/presentation/pages/signin_page.dart
+++ lib/features/auth/presentation/pages/signin_page.dart
@@ -1,10 +1,12 @@
 import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/features/auth/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/auth_field.dart';
 import '../widgets/auth_gradient_button.dart';
 
 class SigninPage extends StatefulWidget {
+  static route() => MaterialPageRoute(builder: (context) => const SigninPage());
   const SigninPage({super.key});
 
   @override
@@ -47,20 +49,25 @@ class _SigninPageState extends State<SigninPage> {
               const SizedBox(height: 20),
               AuthGradientButton(buttonText: "Sign In"),
               SizedBox(height: 20),
-              RichText(
-                  text: TextSpan(
-                      text: "Don't have an account? ",
-                      style: Theme.of(context).textTheme.titleMedium,
-                      children: [
-                    TextSpan(
-                        text: 'Sign Up',
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleMedium
-                            ?.copyWith(
-                                color: AppPallete.gradient2,
-                                fontWeight: FontWeight.bold))
-                  ]))
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, SignupPage.route());
+                },
+                child: RichText(
+                    text: TextSpan(
+                        text: "Don't have an account? ",
+                        style: Theme.of(context).textTheme.titleMedium,
+                        children: [
+                      TextSpan(
+                          text: 'Sign Up',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleMedium
+                              ?.copyWith(
+                                  color: AppPallete.gradient2,
+                                  fontWeight: FontWeight.bold))
+                    ])),
+              )
             ],
           ),
         ),
