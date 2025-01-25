ca31f535077f0f1d8388b605388c11f3f710f953
Temitope <adegtun@yahoo.com>
2025-01-25 13:15:04 +0100

    setting up bloc for sign up and sign in

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -2,6 +2,7 @@ import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:blog_app/features/auth/data/repository/auth_repository_iml.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_reposistory.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_sign_in.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -21,6 +22,7 @@ void _initAuth() {
   serviceLocator.registerFactory<AuthRepository>(
       () => AuthRepositoryImpl(serviceLocator()));
   serviceLocator.registerFactory(() => UserSignUp(serviceLocator()));
-  serviceLocator
-      .registerLazySingleton(() => AuthBloc(userSignUp: serviceLocator()));
+  serviceLocator.registerFactory(() => UserSignIn(serviceLocator()));
+  serviceLocator.registerLazySingleton(() =>
+      AuthBloc(userSignUp: serviceLocator(), userSignIn: serviceLocator()));
 }
